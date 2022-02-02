d23ca819954e2c99ea6577e9b1740ff4e60e98dc
Alexander Sporn <github@alexsporn.de>
2022-02-02 19:18:36 +0100

    - Backported LedgerUpdated event from Stardust and refactored the participation plugin to use that instead

--- pkg/testsuite/coordinator.go
+++ pkg/testsuite/coordinator.go
@@ -86,6 +86,7 @@ func (te *TestEnvironment) configureCoordinator(cooPrivateKeys []ed25519.Private
 			err = te.syncManager.SetConfirmedMilestoneIndex(confirmation.MilestoneIndex, true)
 			require.NoError(te.TestInterface, err)
 		},
+		func(index milestone.Index, newOutputs utxo.Outputs, newSpents utxo.Spents) {},
 		func(index milestone.Index, output *utxo.Output) {},
 		func(index milestone.Index, spent *utxo.Spent) {},
 		nil,
@@ -137,6 +138,11 @@ func (te *TestEnvironment) IssueAndConfirmMilestoneOnTips(tips hornet.MessageIDs
 			}
 			if te.OnConfirmedMilestoneIndexChanged != nil {
 				te.OnConfirmedMilestoneIndexChanged(confirmation.MilestoneIndex)
+			}
+		},
+		func(index milestone.Index, newOutputs utxo.Outputs, newSpents utxo.Spents) {
+			if te.OnLedgerUpdatedFunc != nil {
+				te.OnLedgerUpdatedFunc(index, newOutputs, newSpents)
 			}
 		},
 		func(index milestone.Index, output *utxo.Output) {
